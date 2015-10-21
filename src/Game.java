56bfb6e9949c5a8cd8af70cb017a1bee432a2e6e
mikeberezanich <michaelberezanich@gmail.com>
2015-10-21 18:34:28 -0400

    Added project files

---
+++ src/Game.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+
+public class Game extends JFrame{
+
+	Game(){
+		super();
+		
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+
+	}
+
+}
